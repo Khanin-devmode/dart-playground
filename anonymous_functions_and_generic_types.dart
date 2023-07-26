6e4e24bbfae0d7c29c9a7272ad1d552e27772a00
Khanin Chomphusri <khanin.dev@gmail.com>
2023-07-26 16:34:17 +0700

    firstwhere function

--- anonymous_functions_and_generic_types.dart
+++ anonymous_functions_and_generic_types.dart
@@ -1,9 +1,10 @@
 void main() {
   var list = <int>[1, 2, 3, 4];
-
   var odds = where(list, (x) => x % 2 != 0);
-
   print(odds);
+
+  var isFive = firstWhere(list, (x) => x == 5, orElse: () => -1);
+  print(isFive);
 }
 
 List<T> where<T>(List<T> items, bool Function(T) f) {
@@ -14,4 +15,14 @@ List<T> where<T>(List<T> items, bool Function(T) f) {
     }
   }
   return results;
+}
+
+T firstWhere<T>(List<T> items, bool Function(T) f,
+    {required T Function() orElse}) {
+  for (var item in items) {
+    if (f(item)) {
+      return item;
+    }
+  }
+  return orElse();
 }
